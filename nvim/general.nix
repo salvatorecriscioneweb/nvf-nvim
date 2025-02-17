a8775aa097e751e0e5152ec00a38472300b23149
Salvatore Criscione <not.salvatore@gmail.com>
2025-02-17 13:51:21 +0200

    added nix

--- nvim/general.nix
+++ nvim/general.nix
@@ -2,5 +2,13 @@
   config.vim = {
     filetree.neo-tree.enable = true;
     enableLuaLoader = true;
+
+    languages.nix = {
+      enable = true;
+      format.enable = true;
+      lsp.enable = true;
+      lsp.server = "nixd";
+      treesitter.enable = true;
+    };
   };
 }
