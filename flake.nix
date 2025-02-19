a0521aa1810d8b3496d37fb34f07e540912fb887
Salvatore Criscione <not.salvatore@gmail.com>
2025-02-19 16:41:47 +0000

    feat: added normal pc

--- flake.nix
+++ flake.nix
@@ -5,21 +5,35 @@
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: let
-    salvaConfig = import ./nvim;
-  in {
-    packages.aarch64-linux = {
-      default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-linux;
-          modules = [
-            {
-              config.vim.package = inputs.neovim-nightly-overlay.packages.aarch64-linux.default;
-            }
-            salvaConfig
-          ];
-        })
-        .neovim;
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      salvaConfig = import ./nvim;
+    in
+    {
+      packages.aarch64-linux = {
+        default =
+          (inputs.nvf.lib.neovimConfiguration {
+            pkgs = nixpkgs.legacyPackages.aarch64-linux;
+            modules = [
+              {
+                config.vim.package = inputs.neovim-nightly-overlay.packages.aarch64-linux.default;
+              }
+              salvaConfig
+            ];
+          }).neovim;
+      };
+      packages.x86_64-linux = {
+        default =
+          (inputs.nvf.lib.neovimConfiguration {
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            modules = [
+              {
+                config.vim.package = inputs.neovim-nightly-overlay.packages.x86_64-linux.default;
+              }
+              salvaConfig
+            ];
+          }).neovim;
+      };
     };
-  };
 }
