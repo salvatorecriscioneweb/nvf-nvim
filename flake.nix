a93a551428ed63e1a38f94eb9fa6f67fe26ed7d0
Salvatore Criscione <salvatore.n@grrlz.net>
2025-02-26 11:31:53 +0200

    revert to normal neovim

--- flake.nix
+++ flake.nix
@@ -2,38 +2,30 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs =
-    { nixpkgs, ... }@inputs:
-    let
-      salvaConfig = import ./nvim;
-    in
-    {
-      packages.aarch64-linux = {
-        default =
-          (inputs.nvf.lib.neovimConfiguration {
-            pkgs = nixpkgs.legacyPackages.aarch64-linux;
-            modules = [
-              {
-                config.vim.package = inputs.neovim-nightly-overlay.packages.aarch64-linux.default;
-              }
-              salvaConfig
-            ];
-          }).neovim;
-      };
-      packages.x86_64-linux = {
-        default =
-          (inputs.nvf.lib.neovimConfiguration {
-            pkgs = nixpkgs.legacyPackages.x86_64-linux;
-            modules = [
-              {
-                config.vim.package = inputs.neovim-nightly-overlay.packages.x86_64-linux.default;
-              }
-              salvaConfig
-            ];
-          }).neovim;
-      };
+  outputs = {nixpkgs, ...} @ inputs: let
+    salvaConfig = import ./nvim;
+  in {
+    packages.aarch64-linux = {
+      default =
+        (inputs.nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-linux;
+          modules = [
+            salvaConfig
+          ];
+        })
+        .neovim;
     };
+    packages.x86_64-linux = {
+      default =
+        (inputs.nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            salvaConfig
+          ];
+        })
+        .neovim;
+    };
+  };
 }
